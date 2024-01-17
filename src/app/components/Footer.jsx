f0ac4c21e2d8108f2d39cf6ba356e268ea8478a7
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2024-01-17 15:39:07 +0000

    last modifications

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import LogoImage from "/public/images/logoF2.png";
-import Image from 'next/image';
 
 const Footer = () => {
   return (
     <footer className='footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
-            <Image href={"/"} src={LogoImage} alt="Logo" className="lg:ml-7 h-12 w-12" priority />
+            
             <p className='text-slate-600'>All rights reserved.</p>
         </div>
     </footer>
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,8 +3,7 @@ import React, {useState} from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { MenuOverlay } from "./MenuOverlay";
-import Image from "next/image";
-import LogoImage from "/public/images/logoF2.png"
+
 const navLinks = [
     {
         title: "About",
@@ -15,7 +14,7 @@ const navLinks = [
     {
         title: "Projects",
         path: "#projects",
-        id: "projects",//
+        id: "projects",
     },
     {
         title: "Contact",
@@ -30,8 +29,7 @@ const Navbar = () => {
     return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
         <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-            <Image href={"/"} src={LogoImage} alt="Logo" className="h-10 w-10" priority >
-            </Image>
+           
             <div className="mobile-menu block md:hidden">
                 {!navbarOpen ? (
                     <button onClick={() => setNavbarOpen(true)} 
@@ -67,4 +65,3 @@ const Navbar = () => {
 
 export default Navbar;
 
-console.log (Navbar)
