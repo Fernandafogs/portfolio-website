93e815a334f4a4774aff142b11bea808acc6436f
Fernanda Fogaca <fernanda.ramos.br@gmail.com>
2024-01-17 15:47:45 +0000

    upload new NavBar

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -3,7 +3,8 @@ import React, {useState} from "react";
 import NavLink from "./NavLink";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { MenuOverlay } from "./MenuOverlay";
-
+import Image from "next/image";
+import LogoImage from "/public/images/logoF2.png"
 const navLinks = [
     {
         title: "About",
@@ -29,7 +30,8 @@ const Navbar = () => {
     return (
     <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
         <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
-           
+            <Image href={"/"} src={LogoImage} alt="Logo" className="h-10 w-10" priority >
+            </Image>
             <div className="mobile-menu block md:hidden">
                 {!navbarOpen ? (
                     <button onClick={() => setNavbarOpen(true)} 
@@ -64,4 +66,3 @@ const Navbar = () => {
 }   
 
 export default Navbar;
-
